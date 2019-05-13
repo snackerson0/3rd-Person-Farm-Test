077b4dd214e52d94ab7162888e5468f265f61899
snackerson0 <39027268+snackerson0@users.noreply.github.com>
2019-05-13 19:42:10 -0400

    Fixed looking Up and Down Clamping
    Negative rotation is flipped  back to 360 degress.

--- New 3rd person test/Assets/LookY.cs
+++ New 3rd person test/Assets/LookY.cs
@@ -9,9 +9,29 @@ public class LookY : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 tempAngles = transform.localEulerAngles;
+        while (tempAngles.x > 180)
+        {
+            tempAngles.x -= 360;
+        }
+
+
+        
+
+
+
         float mouseY = Input.GetAxis("Mouse Y");
         Vector3 newRotation = transform.localEulerAngles;
-        newRotation.x -= mouseY * mouseSensitivity;
+        tempAngles.x -= mouseY * mouseSensitivity;
+
+       float mathClamp =  Mathf.Clamp(tempAngles.x, -15.0f, 30.0f);
+        
+        newRotation.x = mathClamp;
+        
         transform.localEulerAngles = newRotation;
+        
+        
+        
+        
     }
 }

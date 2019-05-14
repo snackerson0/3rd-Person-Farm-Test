a247c43a4e0509f0e64a02fa462bea2dc02206b8
snackerson0 <39027268+snackerson0@users.noreply.github.com>
2019-05-14 19:37:51 -0400

    Added Coin Display

--- New 3rd person test/Assets/Currency.cs
+++ New 3rd person test/Assets/Currency.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Currency : MonoBehaviour
 {
     // Start is called before the first frame update
      private int playersBalance;
+    [SerializeField] Text currencyText;
     void Start()
     {
         playersBalance = 0;
+        UpdateCurrencyText();
     }
 
 
@@ -21,11 +23,18 @@ public class Currency : MonoBehaviour
     {
         playersBalance += moneyToAdd;
         print(moneyToAdd + " coins was added");
+        UpdateCurrencyText();
     }
 
     public void DeductMoneyFromPlayer(int moneyToDeduct)
     {
         playersBalance -= moneyToDeduct;
         print(moneyToDeduct + " coins was removed");
+        UpdateCurrencyText();
+    }
+
+   private void UpdateCurrencyText()
+    {
+        currencyText.text = GetPlayersBalance().ToString();
     }
 }
